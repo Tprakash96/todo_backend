a0412de7ac2c017adbaab619cf5653ecc8c1ecf5
vijayacuitusag <vijay.p@acuitusag.com>
2019-08-25 22:39:14 +0530

    database fcredentials chnages

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'Ax1m$0ft@o!7',
+//     database: 'todo'
+// });
+
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Ax1m$0ft@o!7',
-    database: 'todo'
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12303011',
+    password: 'W7I75ZeCsk',
+    database: 'sql12303011'
 });
 
 connection.connect((err) => {
