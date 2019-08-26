44daf62add0212e578c622c0d346a52d7f6a26ca
Tprakash96 <tprakash11121996@gmail.com>
2019-08-27 00:37:18 +0530

    vfb

--- src/helpers/cron-job.js
+++ src/helpers/cron-job.js
@@ -3,8 +3,7 @@ const moment = require('moment-timezone');
 const mail = require('./email');
 
 exports.sendNotificationEmail = (cronTime, emailOptions) => {
-    const time = new Date(cronTime);
-    new CronJob(time, function () {
+    new CronJob(moment(cronTime), function () {
         mail.sendEmail(emailOptions);
     }, null, true, 'Asia/Kolkata');
 }
