8efc71c024db3a56a4a7e035ae8d3ab7c241e5e1
Tprakash96 <tprakash11121996@gmail.com>
2019-08-26 23:33:49 +0530

    UTC time fixed

--- src/helpers/cron-job.js
+++ src/helpers/cron-job.js
@@ -3,7 +3,9 @@ const moment = require('moment-timezone');
 const mail = require('./email');
 
 exports.sendNotificationEmail = (cronTime, emailOptions) => {
-    const momentTime = moment(cronTime);
+    const momentTime = moment(cronTime).add('570', 'minutes');
+    // const momentTime = moment().utc(utcTime);
+    console.log("momentTime", momentTime);
     new CronJob(momentTime, function () {
         mail.sendEmail(emailOptions);
     }, null, true, 'America/Los_Angeles');
